b6d09f68919fed6718137a8e5c1b6284343ed0c1
Umair Abid <umairac90@gmail.com>
2026-05-10 22:12:17 -0700

    Add editor

--- go.mod
+++ go.mod
@@ -6,20 +6,23 @@ require (
 	github.com/charmbracelet/bubbles v0.21.1
 	github.com/charmbracelet/bubbletea v1.3.10
 	github.com/charmbracelet/lipgloss v1.1.0
+	github.com/charmbracelet/x/ansi v0.11.5
 	github.com/jackc/pgx/v5 v5.8.0
+	github.com/kujtimiihoxha/vimtea v0.0.2
 	golang.org/x/term v0.40.0
 )
 
 require (
+	github.com/alecthomas/chroma/v2 v2.15.0 // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/colorprofile v0.4.1 // indirect
-	github.com/charmbracelet/x/ansi v0.11.5 // indirect
 	github.com/charmbracelet/x/cellbuf v0.0.15 // indirect
 	github.com/charmbracelet/x/term v0.2.2 // indirect
 	github.com/clipperhouse/displaywidth v0.9.0 // indirect
 	github.com/clipperhouse/stringish v0.1.1 // indirect
 	github.com/clipperhouse/uax29/v2 v2.5.0 // indirect
+	github.com/dlclark/regexp2 v1.11.4 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
--- internal/editor/editor.go
+++ internal/editor/editor.go
@@ -5,24 +5,50 @@ import (
 	utils "app.lazygit/internal/utils"
 	tea "github.com/charmbracelet/bubbletea"
 	lipgloss "github.com/charmbracelet/lipgloss"
+
+	"github.com/kujtimiihoxha/vimtea"
 )
 
 type EditorModel struct {
 	database adapters.Database
 	layout   utils.ConnectionContainerLayout
 	isActive bool
+	editor   vimtea.Editor
 }
 
 func InitEditor(database adapters.Database, layout utils.ConnectionContainerLayout) EditorModel {
+	lineNumberStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#888888")).
+		Background(lipgloss.Color("#222222")).
+		PaddingRight(1)
+
+	currentLineStyle := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("white")).
+		Background(lipgloss.Color("#444444")).
+		Bold(true).
+		PaddingRight(1)
+
+	cursorStyle := lipgloss.NewStyle().
+		Background(lipgloss.Color("#CC8800")).
+		Foreground(lipgloss.Color("black"))
 	return EditorModel{
 		database: database,
 		layout:   layout,
 		isActive: false,
+		editor: vimtea.NewEditor(
+			vimtea.WithLineNumberStyle(lineNumberStyle),
+			vimtea.WithCurrentLineNumberStyle(currentLineStyle),
+			vimtea.WithCursorStyle(cursorStyle),
+			vimtea.WithRelativeNumbers(true),
+			vimtea.WithContent("Initial content"),
+			vimtea.WithEnableStatusBar(true),
+			vimtea.WithDefaultSyntaxTheme("catppuccin-macchiato"),
+		),
 	}
 }
 
 func (m EditorModel) Init() tea.Cmd {
-	return nil
+	return m.editor.Init()
 }
 
 func (m EditorModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -32,8 +58,13 @@ func (m EditorModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.isActive = string(msg) == "editor"
 	case utils.LayoutUpdated:
 		m.layout = utils.ConnectionContainerLayout(msg)
+		newEditor, sizeCmd := m.editor.SetSize(m.layout.EditorWidth-2, m.layout.EditorHeight-2)
+		m.editor = newEditor.(vimtea.Editor)
+		cmd = sizeCmd
 	}
-	return m, cmd
+	newEditor, newCmd := m.editor.Update(msg)
+	m.editor = newEditor.(vimtea.Editor)
+	return m, tea.Batch(cmd, newCmd)
 }
 
 func (m EditorModel) View() string {
@@ -46,5 +77,5 @@ func (m EditorModel) View() string {
 	if m.isActive {
 		style = style.BorderForeground(lipgloss.Color("205"))
 	}
-	return style.Render("Welcome to LazyGit Editor!\n\nPress q to quit.")
+	return style.Render(m.editor.View())
 }
